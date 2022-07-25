f2b2897824f4ca751897f3641cfbd38abdaf00ee
crater90 <kamranmohd89@gmail.com>
2022-07-25 14:14:33 +0530

    changed firebase credentials

--- firebase.js
+++ firebase.js
@@ -9,18 +9,18 @@ import { getStorage } from 'firebase/storage'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBIITuNMSqDINJCCrwGbW6AN4VZgPOmyn0",
-  authDomain: "admin-daftar.firebaseapp.com",
-  projectId: "admin-daftar",
-  storageBucket: "admin-daftar.appspot.com",
-  messagingSenderId: "767748327169",
-  appId: "1:767748327169:web:e55ecd95cd22c63fb4fd00",
+  apiKey: "AIzaSyDCxPcE-b3rgRPG2sY2VGkcLl_FiJ3cmtc",
+  authDomain: "smart-daftar.firebaseapp.com",
+  projectId: "smart-daftar",
+  storageBucket: "smart-daftar.appspot.com",
+  messagingSenderId: "606701618515",
+  appId: "1:606701618515:web:fcf9aa772d2f0f39450da6",
   measurementId: "${config.measurementId}"
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp;
-const db =  getFirestore();
+const db = getFirestore();
 const storage = getStorage();
 
 export { app, db, storage };
